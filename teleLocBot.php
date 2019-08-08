004156cfac6bdfdd28df02d121d3de53125c1381
七夏浅笑 <smarterq@foxmail.com>
2019-08-08 23:41:28 +0800

    Update teleLocBot.php

--- teleLocBot.php
+++ teleLocBot.php
@@ -13,28 +13,6 @@ if(@$_GET['token'] === $token) {
 	$chat_text = $msg_json['message']['text'];
 	$entities_type_command = $msg_json['message']['entities'][0]['type'];//bot_command
 	
-	/*校验是否IP查询请求*/
-	if(preg_match('/\/iploc/i', $chat_text) && $entities_type_command === "bot_command") {
-		/*校验会话是否授权*/
-		if($chat_username === "scusec") {
-			/*正则匹配提取IP*/
-			preg_match("/[\d\.]{7,15}/",$chat_text,$getip);
-			$ip = $getip[0] ? $getip[0] : '127.0.0.1';
-			if($ip === "127.0.0.1"){
-				$reply = urlencode("IP格式有误");
-			} else {
-				/*请求查询数据*/
-				$ak = md5($ip.$ak);
-				$reply = @file_get_contents("/ipLoc/index.php?ip=".$ip."&ak=".$ak);
-				$reply = $reply ? $reply : '接口调用失败';
-				$reply = urlencode("IP:".$ip."\n".$reply);
-			}
-		} else {
-			$reply = urlencode("此群组或用户未授权");
-		}
-		$callback = @file_get_contents("https://api.telegram.org/bot".$token."/sendMessage?chat_id=".$chat_id."&text=".$reply);
-	}
-	
 	/*校验是否番号查询请求*/
 	if(preg_match('/\/jav/i', $chat_text) && $entities_type_command === "bot_command") {
 		if($chat_username === "scusec") {
